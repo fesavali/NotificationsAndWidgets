876d7f5fcce52943facf5fc0d92abe47f341bf34
fesavali <felixnzioki99@gmail.com>
2021-11-09 16:20:10 +0300

    create notification manager

--- app/src/main/java/com/jwhh/notekeeper/DataManager.kt
+++ app/src/main/java/com/jwhh/notekeeper/DataManager.kt
@@ -27,35 +27,58 @@ object DataManager {
 
         var course = courses["android_intents"]!!
         var note = NoteInfo(course, "Dynamic intent resolution",
-                "Wow, intents allow components to be resolved at runtime")
+            "Wow, intents allow components to be resolved at runtime")
+        note.comments.add(NoteComment("Jeff", "Excellent Point!", System.currentTimeMillis()))
+        note.comments.add(NoteComment("Dave", "We should review this", System.currentTimeMillis() - 5000))
+        note.comments.add(NoteComment("Lucy", "This is good to know", System.currentTimeMillis() - 10000))
         notes.add(note)
         note = NoteInfo(course, "Delegating intents",
-                "PendingIntents are powerful; they delegate much more than just a component invocation")
+            "PendingIntents are powerful; they delegate much more than just a component invocation")
+        note.comments.add(NoteComment("Jeff", "Excellent Point!", System.currentTimeMillis()))
+        note.comments.add(NoteComment("Dave", "We should review this", System.currentTimeMillis() - 5000))
+        note.comments.add(NoteComment("Lucy", "This is good to know", System.currentTimeMillis() - 10000))
         notes.add(note)
 
         course = courses["android_async"]!!
         note = NoteInfo(course, "Service default threads",
-                "Did you know that by default an Android Service will tie up the UI thread?")
+            "Did you know that by default an Android Service will tie up the UI thread?")
+        note.comments.add(NoteComment("Jeff", "Excellent Point!", System.currentTimeMillis()))
+        note.comments.add(NoteComment("Dave", "We should review this", System.currentTimeMillis() - 5000))
+        note.comments.add(NoteComment("Lucy", "This is good to know", System.currentTimeMillis() - 10000))
         notes.add(note)
         note = NoteInfo(course, "Long running operations",
-                "Foreground Services can be tied to a notification icon")
+            "Foreground Services can be tied to a notification icon")
+        note.comments.add(NoteComment("Jeff", "Excellent Point!", System.currentTimeMillis()))
+        note.comments.add(NoteComment("Dave", "We should review this", System.currentTimeMillis() - 5000))
+        note.comments.add(NoteComment("Lucy", "This is good to know", System.currentTimeMillis() - 10000))
         notes.add(note)
 
         course = courses["java_lang"]!!
         note = NoteInfo(course, "Parameters",
-                "Leverage variable-length parameter lists")
+            "Leverage variable-length parameter lists")
+        note.comments.add(NoteComment("Jeff", "Excellent Point!", System.currentTimeMillis()))
+        note.comments.add(NoteComment("Dave", "We should review this", System.currentTimeMillis() - 5000))
+        note.comments.add(NoteComment("Lucy", "This is good to know", System.currentTimeMillis() - 10000))
         notes.add(note)
         note = NoteInfo(course, "Anonymous classes",
-                "Anonymous classes simplify implementing one-use types")
+            "Anonymous classes simplify implementing one-use types")
+        note.comments.add(NoteComment("Jeff", "Excellent Point!", System.currentTimeMillis()))
+        note.comments.add(NoteComment("Dave", "We should review this", System.currentTimeMillis() - 5000))
+        note.comments.add(NoteComment("Lucy", "This is good to know", System.currentTimeMillis() - 10000))
         notes.add(note)
 
         course = courses["java_core"]!!
         note = NoteInfo(course, "Compiler options",
-                "The -jar option isn't compatible with with the -cp option")
+            "The -jar option isn't compatible with with the -cp option")
+        note.comments.add(NoteComment("Jeff", "Excellent Point!", System.currentTimeMillis()))
+        note.comments.add(NoteComment("Dave", "We should review this", System.currentTimeMillis() - 5000))
+        note.comments.add(NoteComment("Lucy", "This is good to know", System.currentTimeMillis() - 10000))
         notes.add(note)
         note = NoteInfo(course, "Serialization",
-                "Remember to include SerialVersionUID to assure version compatibility")
+            "Remember to include SerialVersionUID to assure version compatibility")
+        note.comments.add(NoteComment("Jeff", "Excellent Point!", System.currentTimeMillis()))
+        note.comments.add(NoteComment("Dave", "We should review this", System.currentTimeMillis() - 5000))
+        note.comments.add(NoteComment("Lucy", "This is good to know", System.currentTimeMillis() - 10000))
         notes.add(note)
     }
-
 }
--- app/src/main/java/com/jwhh/notekeeper/ReminderNotification.kt
+++ app/src/main/java/com/jwhh/notekeeper/ReminderNotification.kt
@@ -4,6 +4,7 @@ import android.annotation.TargetApi
 import android.app.Notification
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.app.TaskStackBuilder
 import android.content.Context
 import android.content.Intent
 import android.content.res.Resources
@@ -12,6 +13,8 @@ import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Build
 import androidx.core.app.NotificationCompat
+import androidx.core.app.RemoteInput
+import androidx.core.content.ContextCompat
 
 /**
  * Helper class for showing and canceling reminder
@@ -29,24 +32,52 @@ object ReminderNotification {
 
   const val REMINDER_CHANNEL = "reminders"
 
+  const val KEY_TEXT_REPLY = "keyTextReply"
+
   /**
    * Shows the notification, or updates a previously shown notification of
    * this type, with the given parameters.
    *
    * @see .cancel
    */
-  fun notify(context: Context, titleText: String,
-             noteText: String, number: Int) {
+  fun notify(context: Context, note: NoteInfo, notePosition: Int) {
 
-    val shareIntent = PendingIntent.getActivity(context,
-        0,
-        Intent.createChooser(Intent(Intent.ACTION_SEND)
-            .setType("text/plain")
-            .putExtra(Intent.EXTRA_TEXT,
-                noteText
-            ),
-            "Share Note Reminder"),
+    val intent = Intent(context, NoteActivity::class.java)
+    intent.putExtra(NOTE_POSITION, notePosition)
+
+    val pendingIntent = TaskStackBuilder.create(context)
+        .addNextIntentWithParentStack(intent)
+        .getPendingIntent(0, PendingIntent.FLAG_CANCEL_CURRENT)
+
+    val message1 = NotificationCompat.MessagingStyle.Message(
+        note.comments[0].comment,
+        note.comments[0].timestamp,
+        note.comments[0].name)
+    val message2 = NotificationCompat.MessagingStyle.Message(
+        note.comments[1].comment,
+        note.comments[1].timestamp,
+        note.comments[1].name)
+    val message3 = NotificationCompat.MessagingStyle.Message(
+        note.comments[2].comment,
+        note.comments[2].timestamp,
+        note.comments[2].name)
+
+    val remoteInput = RemoteInput.Builder(KEY_TEXT_REPLY)
+        .setLabel("Add Note")
+        .build()
+
+    val replyIntent = Intent(context, NotificationBroadcastReceiver::class.java)
+    replyIntent.putExtra(NOTE_POSITION, notePosition)
+
+    val replyPendingIntent = PendingIntent.getBroadcast(context,
+        100,
+        replyIntent,
         PendingIntent.FLAG_UPDATE_CURRENT)
+
+    val replyAction = NotificationCompat.Action.Builder(R.drawable.ic_reply_black_24dp,
+        "Add Note", replyPendingIntent)
+        .addRemoteInput(remoteInput)
+        .build()
 
     val builder = NotificationCompat.Builder(context, REMINDER_CHANNEL)
 
@@ -57,8 +88,8 @@ object ReminderNotification {
         // Set required fields, including the small icon, the
         // notification title, and text.
         .setSmallIcon(R.drawable.ic_stat_reminder)
-        .setContentTitle(titleText)
-        .setContentText(noteText)
+        .setContentTitle("Comment from " + note.comments[0].name)
+        .setContentText(note.comments[0].comment)
 
         // All fields below this line are optional.
 
@@ -67,23 +98,28 @@ object ReminderNotification {
         .setPriority(NotificationCompat.PRIORITY_DEFAULT)
 
         // Set ticker text (preview) information for this notification.
-        .setTicker(titleText)
+        .setTicker("Note Comments")
 
         // Set the pending intent to be initiated when the user touches
         // the notification.
-        .setContentIntent(
-            PendingIntent.getActivity(
-                context,
-                0,
-                Intent(context, ItemsActivity::class.java),
-                PendingIntent.FLAG_UPDATE_CURRENT))
+        .setContentIntent(pendingIntent)
 
         // Automatically dismiss the notification when it is touched.
         .setAutoCancel(true)
 
-        // Add a share action
-        .addAction(R.drawable.ic_share_black_24dp,
-            "Share", shareIntent)
+        .setColor(ContextCompat.getColor(context, R.color.darkOrange))
+
+        .setColorized(true)
+
+        .setOnlyAlertOnce(true)
+
+        .setStyle(NotificationCompat.MessagingStyle("You")
+            .setConversationTitle(note.title)
+            .addMessage(message3)
+            .addMessage(message2)
+            .addMessage(message1))
+
+        .addAction(replyAction)
 
     notify(context, builder.build())
   }
